b447c954a96ab517fcc49f8e3fdf5f227e39b139
Denis K <f1nal@cgaming.org>
2015-08-09 18:56:41 +0300

    Fix import

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from distutils.core import setup, find_packages 
+from setuptools import setup, find_packages 
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
