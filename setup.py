c6b76d9f50a1404ab810e3745e1e469f74e1c6b4
Denis K <f1nal@cgaming.org>
2015-08-11 02:20:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     author_email='support@jet.geex-arts.com',
     url='https://github.com/geex-arts/jet',
     packages=find_packages(),
+    license='Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django',
@@ -23,10 +24,14 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Environment :: Web Environment',
         'Topic :: Software Development',
         'Topic :: Software Development :: User Interfaces',
     ],
     zip_safe=False,
-    include_package_data = True
+    include_package_data=True
 )
