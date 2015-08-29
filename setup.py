12420d9fcd808898fd5f52c645a78f6f448e14eb
Denis K <f1nal@cgaming.org>
2015-08-29 23:41:07 +0300

    Prettify setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 import os
 from setuptools import setup, find_packages 
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = 'django-jet',
-    version = __import__('jet').VERSION,
-    description = 'Modern template for Django admin interface with improved functionality',
+    name='django-jet',
+    version=__import__('jet').VERSION,
+    description='Modern template for Django admin interface with improved functionality',
     long_description=read('README.rst'),
-    author = 'Denis Kildishev',
-    author_email = 'support@jet.geex-arts.com',
-    url = 'https://github.com/geex-arts/jet',
+    author='Denis Kildishev',
+    author_email='support@jet.geex-arts.com',
+    url='https://github.com/geex-arts/django-jet',
     packages=find_packages(),
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django',
         'License :: Free for non-commercial use',
@@ -27,5 +28,5 @@ setup(
         'Topic :: Software Development :: User Interfaces',
     ],
     zip_safe=False,
-    include_package_data = True
+    include_package_data=True
 )
