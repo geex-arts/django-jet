f0a724db8efa190af96c324aad9ff6898a6f52eb
Denis K <f1nal@cgaming.org>
2015-10-20 17:14:16 +0300

    Add select2_lookups tests

--- jet/tests/models.py
+++ jet/tests/models.py
@@ -9,3 +9,16 @@ class TestModel(models.Model):
 
     def __str__(self):
         return '%s%d' % (self.field1, self.field2)
+
+
+@python_2_unicode_compatible
+class SearchableTestModel(models.Model):
+    field1 = models.CharField(max_length=255)
+    field2 = models.IntegerField()
+
+    def __str__(self):
+        return '%s%d' % (self.field1, self.field2)
+
+    @staticmethod
+    def autocomplete_search_fields():
+        return 'field1'
