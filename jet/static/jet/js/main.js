bf3d25c07967b2fb8143e7580fd51dadaa43e6d3
Denis K <f1nal@cgaming.org>
2015-10-21 09:37:58 +0300

    Allow global per input lookup

--- jet/static/jet/js/main.js
+++ jet/static/jet/js/main.js
@@ -5,9 +5,12 @@
                 var $select = $(this);
                 var $selectedOption = $select.find('option:selected');
                 var url = $selectedOption.data('url');
+                var querysetLookup = $select.data('queryset--lookup');
 
                 if (url) {
                     document.location = $selectedOption.data('url');
+                } else if (querysetLookup) {
+                    document.location = '?' + querysetLookup + '=' + $selectedOption.val();
                 }
             });
         };
