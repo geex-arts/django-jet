c6aac84b67f62e16e6727477aa0906022d0f9ee0
Yusuf (Josè) Luis <alagunajs@gmail.com>
2016-03-01 14:10:06 -0500

    Fix a warning
    Fix deprecation warnings.

--- jet/dashboard/urls.py
+++ jet/dashboard/urls.py
@@ -1,12 +1,15 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.i18n import javascript_catalog
 from jet.dashboard import dashboard
 from jet.dashboard.views import update_dashboard_modules_view, add_user_dashboard_module_view, \
     update_dashboard_module_collapse_view, remove_dashboard_module_view, UpdateDashboardModuleView, \
     load_dashboard_module_view, reset_dashboard_view
 
-urlpatterns = patterns(
-    '',
+js_info_dict = {
+    'packages': ('jet', ),
+}
+
+urlpatterns = [
     url(
         r'^module/(?P<pk>\d+)/$',
         UpdateDashboardModuleView.as_view(),
@@ -45,9 +48,9 @@ urlpatterns = patterns(
     url(
         r'^jsi18n/$',
         javascript_catalog,
-        {'packages': ('jet',)},
+        js_info_dict,
         name='jsi18n'
     ),
-)
+]
 
 urlpatterns += dashboard.urls.get_urls()
