80f6e8430af254242ab0382a1bd242ab0dffc379
Denis K <f1nal@cgaming.org>
2016-07-23 12:27:12 +0300

    Add sibling buttons to change form

--- jet/settings.py
+++ jet/settings.py
@@ -7,3 +7,6 @@ JET_THEMES = getattr(settings, 'JET_THEMES', [])
 # Side menu
 JET_SIDE_MENU_COMPACT = getattr(settings, 'JET_SIDE_MENU_COMPACT', False)
 JET_SIDE_MENU_CUSTOM_APPS = getattr(settings, 'JET_SIDE_MENU_CUSTOM_APPS', None)
+
+# Improved usability
+JET_CHANGE_FORM_SIBLING_LINKS = getattr(settings, 'JET_CHANGE_FORM_SIBLING_LINKS', True)
