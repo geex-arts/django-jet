a2b2f65e600926ca7b796694db2a5924be6b55d5
Denis K <f1nal@cgaming.org>
2016-07-24 14:44:08 +0300

    Fixes RelatedObjectLookups for django 1.9+

--- jet/static/admin/js/admin/RelatedObjectLookups.js
+++ jet/static/admin/js/admin/RelatedObjectLookups.js
@@ -176,3 +176,29 @@ function closeRelatedPopup(win) {
     jet.jQuery('select').trigger('select:init');
     jet.jQuery(win.parent).trigger('related-popup:close');
 }
+
+django.jQuery(document).ready(function() {
+    jet.jQuery(function($){
+        function updateLinks() {
+            var $this = $(this);
+            var siblings = $this.nextAll('.change-related, .delete-related');
+            if (!siblings.length) return;
+            var value = $this.val();
+            if (value) {
+                siblings.each(function(){
+                    var elm = $(this);
+                    elm.attr('href', elm.attr('data-href-template').replace('__fk__', value));
+                });
+            } else siblings.removeAttr('href');
+        }
+        var container = $(document);
+        container.on('change', '.related-widget-wrapper select', updateLinks);
+        container.find('.related-widget-wrapper select').each(updateLinks);
+        container.on('click', '.related-widget-wrapper-link', function(event){
+            if (this.href) {
+                showRelatedObjectPopup(this);
+            }
+            event.preventDefault();
+        });
+    });
+});
