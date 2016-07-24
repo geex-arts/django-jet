f884b873950dbb656aa84615f03b4649131ae838
Denis K <f1nal@cgaming.org>
2016-07-24 16:07:22 +0300

    Fix concatenate

--- jet/tests/settings.py
+++ jet/tests/settings.py
@@ -28,7 +28,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+TEMPLATE_CONTEXT_PROCESSORS = tuple(global_settings.TEMPLATE_CONTEXT_PROCESSORS) + (
     'django.core.context_processors.request',
 )
 
