ff2b38c3f0907ad7cef638809599699945ff992b
Denis K <f1nal@cgaming.org>
2016-08-08 21:43:49 +0300

    Refactor dashboard js

--- jet/static/jet/js/src/features/dashboard.js
+++ jet/static/jet/js/src/features/dashboard.js
@@ -12,12 +12,94 @@ require('jquery-ui/ui/resizable');
 require('jquery-ui/ui/button');
 require('jquery-ui/ui/dialog');
 
-var initDashboard = function() {
-    var updateDashboardModules = function () {
-        var $form = $('#update-dashboard-modules-form');
+var Dashboard = function($dashboard) {
+    this.$dashboard = $dashboard;
+};
+
+Dashboard.prototype = {
+    t: function(s) {
+        if (window.django == undefined) {
+            return s;
+        }
+        return django.gettext(s);
+    },
+    initTools: function($dashboard) {
+        var self = this;
+
+        $dashboard.find('.dashboard-tools-toggle').on('click', function () {
+            $dashboard.find('.dashboard-tools').toggleClass('visible');
+        });
+
+        var $form = $dashboard.find('#add-dashboard-module-form');
+
+        $form.find('.add-dashboard-link').on('click', function (e) {
+            var $typeInput = $form.find('[name="type"]');
+            var type = $form.find('[name="module"] option:selected').data('type');
+
+            if (type) {
+                $typeInput.val(type);
+
+                $.ajax({
+                    url: $form.attr('action'),
+                    method: $form.attr('method'),
+                    dataType: 'json',
+                    data: $form.serialize(),
+                    success: function (result) {
+                        if (result.error) {
+                            return;
+                        }
+
+                        document.location = result.success_url;
+                    }
+                });
+            }
+
+            e.preventDefault();
+        });
+
+        $dashboard.find('.reset-dashboard-link').on('click', function(e) {
+            var buttons = {};
+            var resetDashboard = function () {
+                var $form = $dashboard.find('#reset-dashboard-form');
+
+                $.ajax({
+                    url: $form.attr('action'),
+                    method: $form.attr('method'),
+                    dataType: 'json',
+                    data: $form.serialize(),
+                    success: function (result) {
+                        if (result.error) {
+                            return;
+                        }
+
+                        location.reload();
+                    }
+                });
+            };
+
+            buttons[self.t('Yes')] = function() {
+                resetDashboard();
+                $(this).dialog('close');
+            };
+
+            buttons[self.t('Cancel')] = function() {
+                $(this).dialog('close');
+            };
+
+            $dashboard.find('#reset-dashboard-dialog').dialog({
+                resizable: false,
+                modal: true,
+                buttons: buttons
+            });
+
+            e.preventDefault();
+        });
+    },
+    updateDashboardModules: function($dashboard) {
+        var $form = $dashboard.find('#update-dashboard-modules-form');
         var modules = [];
 
-        $('.dashboard-column').each(function () {
+        $dashboard.find('.dashboard-column').each(function () {
             var $column = $(this);
             var column = $column.closest('.dashboard-column-wrapper').index();
 
@@ -42,72 +124,51 @@ var initDashboard = function() {
             dataType: 'json',
             data: $form.serialize()
         });
-    };
+    },
+    initModulesDragAndDrop: function($dashboard) {
+        var self = this;
 
-    $('.dashboard-column').droppable({
-        activeClass: 'active',
-        hoverClass: 'hovered',
-        tolerance: 'pointer',
-        accept: '.dashboard-item'
-    }).sortable({
-        items: '.dashboard-item',
-        handle: '.dashboard-item-header',
-        tolerance: 'pointer',
-        connectWith: '.dashboard-column',
-        cursor: 'move',
-        placeholder: 'dashboard-item placeholder',
-        forcePlaceholderSize: true,
-        update: function (event, ui) {
-            updateDashboardModules();
-        }
-    });
-
-    $('.dashboard-item.collapsible').each(function () {
-        var $item = $(this);
-        var $link = $item.find('.dashboard-item-collapse');
-        var $collapsible = $item.find('.dashboard-item-content');
-        var $form = $('#update-dashboard-module-collapse-form');
-        var moduleId = $item.data('module-id');
-
-        $link.on('click', function (e) {
-            e.preventDefault();
-
-            $collapsible.slideFadeToggle(200, 'swing', function () {
-                var collapsed = $collapsible.is(':visible') == false;
-
-                if (collapsed) {
-                    $item.addClass('collapsed')
-                } else {
-                    $item.removeClass('collapsed')
-                }
-
-                $form.find('[name="id"]').val(moduleId);
-                $form.find('[name="collapsed"]').val(collapsed ? 'true' : 'false');
-
-                $.ajax({
-                    url: $form.attr('action'),
-                    method: $form.attr('method'),
-                    dataType: 'json',
-                    data: $form.serialize()
-                });
-            });
+        $dashboard.find('.dashboard-column').droppable({
+            activeClass: 'active',
+            hoverClass: 'hovered',
+            tolerance: 'pointer',
+            accept: '.dashboard-item'
+        }).sortable({
+            items: '.dashboard-item',
+            handle: '.dashboard-item-header',
+            tolerance: 'pointer',
+            connectWith: '.dashboard-column',
+            cursor: 'move',
+            placeholder: 'dashboard-item placeholder',
+            forcePlaceholderSize: true,
+            update: function (event, ui) {
+                self.updateDashboardModules($dashboard);
+            }
         });
-    });
+    },
+    initCollapsibleModules: function($dashboard) {
+        var $form = $dashboard.find('#update-dashboard-module-collapse-form');
 
-    $('.dashboard-item.deletable').each(function () {
-        var $item = $(this);
-        var $link = $item.find('.dashboard-item-remove');
-        var $form = $('#remove-dashboard-module-form');
-        var moduleId = $item.data('module-id');
+        $dashboard.find('.dashboard-item.collapsible').each(function () {
+            var $item = $(this);
+            var $link = $item.find('.dashboard-item-collapse');
+            var $collapsible = $item.find('.dashboard-item-content');
+            var moduleId = $item.data('module-id');
 
-        $link.on('click', function (e) {
-            e.preventDefault();
+            $link.on('click', function (e) {
+                e.preventDefault();
 
-            var buttons = {};
+                $collapsible.slideFadeToggle(200, 'swing', function () {
+                    var collapsed = $collapsible.is(':visible') == false;
 
-            var deleteModule = function () {
-                $item.fadeOut(200, 'swing', function () {
+                    if (collapsed) {
+                        $item.addClass('collapsed')
+                    } else {
+                        $item.removeClass('collapsed')
+                    }
+
                     $form.find('[name="id"]').val(moduleId);
+                    $form.find('[name="collapsed"]').val(collapsed ? 'true' : 'false');
 
                     $.ajax({
                         url: $form.attr('action'),
@@ -116,127 +177,102 @@ var initDashboard = function() {
                         data: $form.serialize()
                     });
                 });
-            };
-
-            buttons[django.gettext('Delete')] = function () {
-                deleteModule();
-                $(this).dialog('close');
-            };
-
-            buttons[django.gettext('Cancel')] = function () {
-                $(this).dialog('close');
-            };
-
-            $('#module-remove-dialog').dialog({
-                resizable: false,
-                modal: true,
-                buttons: buttons
             });
         });
-    });
+    },
+    initDeletableModules: function($dashboard) {
+        var self = this;
+        var $form = $dashboard.find('#remove-dashboard-module-form');
 
-    $('.dashboard-tools-toggle').on('click', function() {
-        $('.dashboard-tools').toggleClass('visible');
-    });
+        $dashboard.find('.dashboard-item.deletable').each(function () {
+            var $item = $(this);
+            var $link = $item.find('.dashboard-item-remove');
+            var moduleId = $item.data('module-id');
 
-    var $form = $('#add-dashboard-module-form');
+            $link.on('click', function (e) {
+                e.preventDefault();
 
-    $form.find('.add-dashboard-link').on('click', function (e) {
-        var $typeInput = $form.find('[name="type"]');
-        var type = $form.find('[name="module"] option:selected').data('type');
+                var buttons = {};
 
-        if (type) {
-            $typeInput.val(type);
+                var deleteModule = function () {
+                    $item.fadeOut(200, 'swing', function () {
+                        $form.find('[name="id"]').val(moduleId);
+
+                        $.ajax({
+                            url: $form.attr('action'),
+                            method: $form.attr('method'),
+                            dataType: 'json',
+                            data: $form.serialize()
+                        });
+                    });
+                };
+
+                buttons[self.t('Delete')] = function () {
+                    deleteModule();
+                    $(this).dialog('close');
+                };
+
+                buttons[self.t('Cancel')] = function () {
+                    $(this).dialog('close');
+                };
+
+                $dashboard.find('#module-remove-dialog').dialog({
+                    resizable: false,
+                    modal: true,
+                    buttons: buttons
+                });
+            });
+        });
+    },
+    initAjaxModules: function($dashboard) {
+        $dashboard.find('.dashboard-item.ajax').each(function () {
+            var $item = $(this);
+            var $content = $item.find('.dashboard-item-content');
+            var url = $item.data('ajax-url');
 
             $.ajax({
-                url: $form.attr('action'),
-                method: $form.attr('method'),
+                url: url,
                 dataType: 'json',
-                data: $form.serialize(),
                 success: function (result) {
                     if (result.error) {
+                        $content.empty();
                         return;
                     }
 
-                    document.location = result.success_url;
+                    var oldHeight = $content.height();
+                    $content.html(result.html);
+                    var newHeight = $content.height();
+
+                    $content.height(oldHeight);
+                    $content.animate({
+                        height: newHeight
+                    }, 250);
+                },
+                error: function () {
+                    $content.empty();
                 }
             });
+        });
+    },
+    run: function() {
+        var $dashboard = this.$dashboard;
+
+        try {
+            this.initTools($dashboard);
+            this.initModulesDragAndDrop($dashboard);
+            this.initCollapsibleModules($dashboard);
+            this.initDeletableModules($dashboard);
+            this.initAjaxModules($dashboard);
+        } catch (e) {
+            console.error(e, e.stack);
         }
 
-        e.preventDefault();
-    });
-
-    $('.dashboard-item.ajax').each(function () {
-        var $item = $(this);
-        var $content = $item.find('.dashboard-item-content');
-        var url = $item.data('ajax-url');
-        var moduleId = $item.data('module-id');
-
-        $form.find('[name="id"]').val(moduleId);
-
-        $.ajax({
-            url: url,
-            dataType: 'json',
-            success: function (result) {
-                if (result.error) {
-                    $content.empty();
-                    return;
-                }
-
-                var oldHeight = $content.height();
-                $content.html(result.html);
-                var newHeight = $content.height();
-
-                $content.height(oldHeight);
-                $content.animate({
-                    height: newHeight
-                }, 250);
-            },
-            error: function() {
-                $content.empty();
-            }
-        });
-    });
-
-    $('.reset-dashboard-link').on('click', function(e) {
-        var buttons = {};
-        var resetDashboard = function () {
-            var $form = $('#reset-dashboard-form');
-
-            $.ajax({
-                url: $form.attr('action'),
-                method: $form.attr('method'),
-                dataType: 'json',
-                data: $form.serialize(),
-                success: function (result) {
-                    if (result.error) {
-                        return;
-                    }
-
-                    location.reload();
-                }
-            });
-        };
-
-        buttons[django.gettext('Yes')] = function() {
-            resetDashboard();
-            $(this).dialog('close');
-        };
-
-        buttons[django.gettext('Cancel')] = function() {
-            $(this).dialog('close');
-        };
-
-        $('#reset-dashboard-dialog').dialog({
-            resizable: false,
-            modal: true,
-            buttons: buttons
-        });
-
-        e.preventDefault();
-    });
+        $dashboard.addClass('initialized');
+    }
 };
 
 $(document).ready(function() {
-    initDashboard();
+    $('.dashboard.jet').each(function() {
+        new Dashboard($(this)).run();
+    });
 });
