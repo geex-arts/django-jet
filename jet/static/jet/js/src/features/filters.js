a2df7f763c6a6be771852a542e1f26ac439e4940
Denis K <f1nal@cgaming.org>
2016-08-09 21:20:29 +0300

    Refactor filers js

--- jet/static/jet/js/src/features/filters.js
+++ jet/static/jet/js/src/features/filters.js
@@ -1,20 +1,35 @@
 var $ = require('jquery');
 
-var initFilters = function() {
-    $('.changelist-filter-select').on('change', function () {
-        var $select = $(this);
-        var $selectedOption = $select.find('option:selected');
-        var url = $selectedOption.data('url');
-        var querysetLookup = $select.data('queryset--lookup');
+var Filters = function($toolbar) {
+    this.$toolbar = $toolbar;
+};
 
-        if (url) {
-            document.location = $selectedOption.data('url');
-        } else if (querysetLookup) {
-            document.location = '?' + querysetLookup + '=' + $selectedOption.val();
+Filters.prototype = {
+    initFiltersInteraction: function($toolbar) {
+        $toolbar.find('.changelist-filter-select').on('change', function () {
+            var $select = $(this);
+            var $selectedOption = $select.find('option:selected');
+            var url = $selectedOption.data('url');
+            var querysetLookup = $select.data('queryset--lookup');
+
+            if (url) {
+                document.location = $selectedOption.data('url');
+            } else if (querysetLookup) {
+                document.location = '?' + querysetLookup + '=' + $selectedOption.val();
+            }
+        });
+    },
+    run: function() {
+        try {
+            this.initFiltersInteraction(this.$toolbar);
+        } catch (e) {
+            console.error(e, e.stack);
         }
-    });
+    }
 };
 
 $(document).ready(function() {
-    initFilters();
+    $('#toolbar').each(function() {
+        new Filters($(this)).run();
+    });
 });
