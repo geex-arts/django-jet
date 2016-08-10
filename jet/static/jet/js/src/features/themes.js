7af6eb553f8508af426a4926057bc8079bbf25d7
Denis K <f1nal@cgaming.org>
2016-08-10 15:10:42 +0300

    Implement themes

--- jet/static/jet/js/src/features/themes.js
+++ jet/static/jet/js/src/features/themes.js
@@ -2,45 +2,61 @@ require('jquery.cookie');
 
 var $ = require('jquery');
 
-var initThemeChoosing = function() {
-    $('.choose-theme').on('click', function () {
-        var $link = $(this);
+var Themes = function() { };
 
-        $.cookie('JET_THEME', $link.data('theme'), { expires: 365, path: '/' });
+Themes.prototype = {
+    moveChooser: function() {
+        var $chooser = $('.theme-chooser').detach();
+        $('.user-tools-welcome-msg').after($chooser);
+    },
+    initChooser: function() {
+        $('.choose-theme').on('click', function () {
+            var $link = $(this);
 
-        var cssToLoad = [
-            { url: $link.data('base-stylesheet'), class: 'base-stylesheet' },
-            { url: $link.data('select2-stylesheet'), class: 'select2-stylesheet' },
-            { url: $link.data('jquery-ui-stylesheet'), class: 'jquery-ui-stylesheet' }
-        ];
+            $.cookie('JET_THEME', $link.data('theme'), { expires: 365, path: '/' });
 
-        var loadedCss = 0;
+            var cssToLoad = [
+                { url: $link.data('base-stylesheet'), class: 'base-stylesheet' },
+                { url: $link.data('select2-stylesheet'), class: 'select2-stylesheet' },
+                { url: $link.data('jquery-ui-stylesheet'), class: 'jquery-ui-stylesheet' }
+            ];
 
-        var onCssLoaded = function() {
-            ++loadedCss;
+            var loadedCss = 0;
 
-            if (loadedCss == cssToLoad.length) {
-                $(document).trigger('theme:changed');
-            }
-        };
+            var onCssLoaded = function() {
+                ++loadedCss;
 
-        cssToLoad.forEach(function(css) {
-            $('<link>')
-                .attr('rel', 'stylesheet')
-                .addClass(css['class'])
-                .attr('href', css['url'])
-                .load(onCssLoaded)
-                .appendTo('head');
-            $('.' + css['class'])
-                .slice(0, -2)
-                .remove();
+                if (loadedCss == cssToLoad.length) {
+                    $(document).trigger('theme:changed');
+                }
+            };
+
+            cssToLoad.forEach(function(css) {
+                $('<link>')
+                    .attr('rel', 'stylesheet')
+                    .addClass(css['class'])
+                    .attr('href', css['url'])
+                    .load(onCssLoaded)
+                    .appendTo('head');
+                $('.' + css['class'])
+                    .slice(0, -2)
+                    .remove();
+            });
+
+            $('.choose-theme').removeClass('selected');
+            $link.addClass('selected');
         });
-
-        $('.choose-theme').removeClass('selected');
-        $link.addClass('selected');
-    });
+    },
+    run: function() {
+        try {
+            this.moveChooser();
+            this.initChooser();
+        } catch (e) {
+            console.error(e, e.stack);
+        }
+    }
 };
 
 $(document).ready(function() {
-    initThemeChoosing();
+    new Themes().run();
 });
