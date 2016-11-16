78bc2d2b24d83bdf1c9dca14dbdffd306792177f
Michael Kutý <6du1ro.n@gmail.com>
2016-11-16 12:08:44 +0100

    Fix rebase.

--- jet/dashboard/templatetags/jet_dashboard_tags.py
+++ jet/dashboard/templatetags/jet_dashboard_tags.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django import template
+from django.core.urlresolvers import resolve
 from jet.dashboard.utils import get_current_dashboard
 
 register = template.Library()
@@ -9,7 +10,8 @@ register = template.Library()
 def get_dashboard(context, location):
     dashboard_cls = get_current_dashboard(location)
 
-    app_label = context['request'].resolver_match.kwargs.get('app_label')
+    resolver = resolve(context['request'].path)
+    app_label = resolver.kwargs.get('app_label')
 
     return dashboard_cls(context, app_label=app_label)
 
