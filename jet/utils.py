bea1de00f7b959cb4f6d08e6098c01ae1ea2796c
Takeshi Kanemoto <tak.kanemoto@gmail.com>
2018-06-17 13:14:03 +0100

    Fix get_model_queryset exception when model_admin is None

--- jet/utils.py
+++ jet/utils.py
@@ -180,6 +180,9 @@ class SuccessMessageMixin(object):
 def get_model_queryset(admin_site, model, request, preserved_filters=None):
     model_admin = admin_site._registry.get(model)
 
+    if model_admin is None:
+        return
+
     try:
         changelist_url = reverse('%s:%s_%s_changelist' % (
             admin_site.name,
