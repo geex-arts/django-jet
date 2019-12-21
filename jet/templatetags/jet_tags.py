fd5d277e5ffac1071da422ed5af213e596841d0a
slavaromanov <monodry@gmail.com>
2019-12-21 19:10:46 +0300

    add test_jet_have_admin_static

--- jet/templatetags/jet_tags.py
+++ jet/templatetags/jet_tags.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 import json
 import os
+import django
 from django import template
 try:
     from django.core.urlresolvers import reverse
@@ -12,7 +13,6 @@ from django.contrib.admin.widgets import RelatedFieldWidgetWrapper
 from django.utils.formats import get_format
 from django.utils.safestring import mark_safe
 from django.utils.encoding import smart_text
-from django import VERSION
 from jet import settings, VERSION
 from jet.models import Bookmark
 from jet.utils import get_model_instance_label, get_model_queryset, get_possible_language_codes, \
@@ -257,4 +257,4 @@ def jet_static_translation_urls():
 
 @register.filter
 def jet_have_admin_static():
-    return VERSION[0] < 3
+    return django.VERSION[0] < 3
