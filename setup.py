d7dd77323d8055b46599c47037308a7125d83509
Barukimang <pieterbuikema@gmail.com>
2020-02-26 21:59:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_install_requires():
 
 setup(
     name='django-3-jet',
-    version=__import__('jet').VERSION,
+    version='1.0.8',
     description='Modern template for Django admin interface with improved functionality',
     long_description=read('README.rst'),
     author='Denis Kildishev',
