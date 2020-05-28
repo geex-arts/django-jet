022e0055fd748f61693bd45e144a0be5db40feff
rafaelhenter <rafael@henter.com.br>
2020-05-28 13:06:49 -0300

    Bugfix to add Django 3 compatibility

--- jet/models.py
+++ jet/models.py
@@ -1,6 +1,7 @@
+from six import python_2_unicode_compatible
+
 from django.db import models
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
