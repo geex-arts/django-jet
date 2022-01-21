d7048eaab178ed8710e2564311ba080dd2669017
Sunday Ajayi <sunnexajayi@gmail.com>
2022-01-21 16:05:44 +0100

    Modified the package for importing from django.utils.encoding to six

--- jet/models.py
+++ jet/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
+from six import python_2_unicode_compatible
 
+from django.utils.translation import ugettext_lazy as _
 
 @python_2_unicode_compatible
 class Bookmark(models.Model):
